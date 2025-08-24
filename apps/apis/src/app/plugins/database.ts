8929cf1c11f1f8734113781e253be34082861ed8
Huy Do Quang <huy.do.quang@advesa.com>
2025-08-24 20:42:14 +0700

    add auth

--- apps/apis/src/app/plugins/database.ts
+++ apps/apis/src/app/plugins/database.ts
@@ -1,18 +1,15 @@
 import fp from 'fastify-plugin'
 import { FastifyPluginAsync } from 'fastify'
 import { prisma } from '../../lib/database'
-import { supabase } from '../../lib/supabase'
 
 declare module 'fastify' {
   interface FastifyInstance {
     prisma: typeof prisma
-    supabase: typeof supabase
   }
 }
 
 const databasePlugin: FastifyPluginAsync = async (fastify) => {
   fastify.decorate('prisma', prisma)
-  fastify.decorate('supabase', supabase)
 
   fastify.addHook('onClose', async () => {
     await prisma.$disconnect()
